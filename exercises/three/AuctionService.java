784b69797baf735b6894bb0eeb6aff3b4dc107cf
dohatch <dohatch@student.neumont.edu>
2014-10-09 12:48:05 -0600

    Finished Assignment 3
    Finished assignment three and Added a jar to the Lab

---
+++ exercises/three/AuctionService.java
@@ -0,0 +1,16 @@
+package three;
+
+public interface AuctionService {
+
+	public Auction[] search(String criteria);
+
+	public void bid(String username, int itemId);
+	
+	public Auction create(Auction auction);
+	
+	public Auction retreive(Integer id);
+	
+	public Auction update(Auction auction, Integer id);
+	
+	public void delete(Integer id);
+}
